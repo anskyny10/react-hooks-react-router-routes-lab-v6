8d3368da2e5bc97b8d2b7864c12168951a7dfd24
Jason Malefakis <anskyny10@aol.com>
2024-07-28 23:20:28 -0400

    movie project

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,37 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
-  return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Actor info here! */}
-      </main>
-    </>
-  );
-};
+  
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/actors')
+    .then(res => res.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+return (
+  <>
+    <header>
+      {<NavBar />}
+    </header>
+    <main>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <article key={actor.id}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </main>
+  </>
+);
+}
 
 export default Actors;
